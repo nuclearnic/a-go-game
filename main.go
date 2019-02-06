7f0c98bb5726b67b83ce88051276eaf1e46879c0
nuclearnic <nuclearnicdev@gmail.com>
2019-02-06 22:57:10 +0200

    Render window

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/ttf"
 )
 
 func main() {
@@ -21,15 +22,53 @@ func run() error {
 	}
 	defer sdl.Quit()
 
-	w, r, err := sdl.CreateWindowAndRenderer(800, 600, sdl.WINDOWEVENT_SHOWN)
+	ttf.Init()
+	if err := ttf.Init(); err != nil {
+		return fmt.Errorf("could not initialize TTF: %v", err)
+	}
+	defer ttf.Quit()
+
+	w, r, err := sdl.CreateWindowAndRenderer(100, 100, sdl.WINDOWEVENT_SHOWN)
 	if err != nil {
 		return fmt.Errorf("could not create window: %v", err)
 	}
 	defer w.Destroy()
 
-	_ = r
+	if err := drawTitle(r); err != nil {
+		return fmt.Errorf("could not draw title: %v", err)
+	}
 
-	time.Sleep(5 * time.Second)
+	time.Sleep(3 * time.Second)
+
+	return nil
+}
+
+func drawTitle(r *sdl.Renderer) error {
+	r.Clear()
+
+	f, err := ttf.OpenFont("res/fonts/Pacifico.ttf", 15)
+	if err != nil {
+		return fmt.Errorf("could not load font: %v", err)
+	}
+	defer f.Close()
+
+	c := sdl.Color{R: 255, G: 100, B: 0, A: 255}
+	s, err := f.RenderUTF8Solid("Flappy Gopher", c)
+	if err != nil {
+		return fmt.Errorf("could not render title: %v", err)
+	}
+	defer s.Free()
+
+	t, err := r.CreateTextureFromSurface(s)
+	if err != nil {
+		return fmt.Errorf("could not create texture: %v", err)
+	}
+	defer t.Destroy()
+
+	if err := r.Copy(t, nil, nil); err != nil {
+		return fmt.Errorf("could not copy texture: %v", err)
+	}
+	r.Present()
 
 	return nil
 }
